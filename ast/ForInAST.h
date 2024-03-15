eeec4edbbb025219353342d786a994d0e60419be
andreigarbo <andrei.garbo@gmail.com>
2024-03-15 08:33:20 +0200

    Added variable assignment parser

--- ast/ForInAST.h
+++ ast/ForInAST.h
@@ -4,14 +4,14 @@
 #include <GenericAST.h>
 
 class ForInAST : public GenericAST {
-    unique_ptr<GenericAST> term, array, body;
+    unique_ptr<GenericAST> term, iterable, body;
 
     public:
         ForInAST(
             unique_ptr<GenericAST> term,
-            unique_ptr<GenericAST> array,
+            unique_ptr<GenericAST> iterable,
             unique_ptr<GenericAST> body
-        ) : term(std::move(term)), array(std::move(array)), body(std::move(body)) {}
+        ) : term(std::move(term)), iterable(std::move(iterable)), body(std::move(body)) {}
         Value* codegen() {}
 }
 
